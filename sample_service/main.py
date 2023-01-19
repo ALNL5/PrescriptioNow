e5a06b2392d0b42cb6f53ebf3caf196cd7e0f235
Grace Strutzel <gmstrutzel@icloud.com>
2023-01-19 15:29:55 -0800

    removed node modules and packages

--- sample_service/main.py
+++ sample_service/main.py
@@ -6,9 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000")
-    ],
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -24,6 +22,6 @@ def launch_details():
             "day": "9",
             "hour": 19,
             "min": 0,
-            "tz:": "PST"
+            "tz:": "PST",
         }
     }
