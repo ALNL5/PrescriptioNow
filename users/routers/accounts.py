3812e1699afa87487dc8543acd9c32511c19e7cd
Grace Strutzel <gmstrutzel@icloud.com>
2023-01-24 08:39:53 -0800

    Backend authentication complete

--- users/routers/accounts.py
+++ users/routers/accounts.py
@@ -36,6 +36,26 @@ class HttpError(BaseModel):
 router = APIRouter()
 
 
+@router.get("/api/protected", response_model=bool)
+async def get_protected(
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    return True
+
+
+@router.get("/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    account: AccountOut = Depends(authenticator.try_get_current_account_data)
+) -> AccountToken | None:
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": account,
+        }
+
+
 @router.post("/api/accounts", response_model=AccountToken | HttpError)
 async def create_account(
     info: AccountIn,
