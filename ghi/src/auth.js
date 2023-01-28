5942ab3ec16e9480596affea30a421a13e8e46d2
Jared Aiden Wolf <jared@jaredaidenwolf.com>
2023-01-27 22:04:15 -0500

    update auth.js

--- ghi/src/auth.js
+++ ghi/src/auth.js
@@ -7,7 +7,7 @@ export function getToken() {
 }
 
 export async function getTokenInternal() {
-  const url = `${process.env.REACT_APP_USER_API_HOST}/token/`;
+  const url = `${process.env.REACT_APP_USER_API_HOST}/token`;
   try {
     const response = await fetch(url, {
       credentials: "include",
